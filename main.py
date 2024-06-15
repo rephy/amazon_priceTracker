a63e58a12d2756ff5695ef6b5ef240086e55be4d
Raphael Manayon <rephy@Raphaels-Air.lan>
2024-06-14 19:19:41 -0700

    V1 - Send email on good deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import lxml
+from sender import EmailSender
 from bs4 import BeautifulSoup
 
 URL = "https://www.amazon.com/Texas-Instruments-Calculator-Certified-Refurbished/dp/B07HG3WGVY/ref=sr_1_5?crid=1ZF5CSK6YN5MN&dib=eyJ2IjoiMSJ9.TWJeX2tZXfhY2G23qRid3MxzrcCbjTpNq1EuXVJJO-wpQovXhnMULy6sbcyNsPgXC0szT9QUOe2TZdEikbFQVjl6xpRrWG4vkluklnkriBcUyb5dSP8QCpSrOmFDDcD5H0e5DdjwDQBB_qMMuXQe4Vfs7adXCYKzse6rInN9yVu4ClN_fc1YbLCqgBzd-bQGF6lzz2oZtfyQgDL4XSi-2jYvo5CqRbI3YyUKA-8euiE.eTI8ZWJyc8Z8W9vOE3KEs8hA5O1xcolvKg026PIZfq8&dib_tag=se&keywords=ti-84&qid=1718416133&sprefix=ti-84%2Caps%2C203&sr=8-5" # Replace with URL of your desired Amazon product
@@ -12,4 +13,9 @@ price = float(soup.select_one("td span.a-offscreen").getText()[1:]) # in USD
 print(price)
 
 if price <= good_price:
-    print("Good deal!")
+    sender = EmailSender() # Type your Gmail address and Google Account's app password
+    sender.connect()
+    sender.compose(target="unitsocial@zohomail.com", from_name="TI-84 Price Tracker", to_name="You", subject="Great Amazon Deals for a TI-84 On Amazon",
+                   message=f"The TI-84 Graphing Calculator on Amazon is only ${price}! Buy it while you can!") # Feel free to play around with this
+    sender.send()
+    sender.close()
